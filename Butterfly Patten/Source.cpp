fed801e775ce51ab59edcb245550ab10022630ae
wittawas19@gmail.com <wittawas19@gmail.com>
2020-09-03 00:03:34 +0700

    Complete

--- Butterfly Patten/Source.cpp
+++ Butterfly Patten/Source.cpp
@@ -1,26 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int n , j;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		for ( j = 0; j <= i; j++)
-		{
-			printf("*"); 
-		}
-		for ( int k = 0; k <= 2*n - 1 ; k++)
-		{
-			printf(" ");
-		}
-		
-		for ( j = i ; j >= 1 ; j--)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-	return 0;
-
+    int n ;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int k = 1; k <= 2 * (n - (i+1)); k++)
+        {
+            printf(" ");
+        }
+        printf("\b");
+        for (int j = i; j >= 0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n-2; i >= 0 ; i--)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int k = 1; k <= 2 * (n - (i + 1)); k++)
+        {
+            printf(" ");
+        }
+        printf("\b");
+        for (int j = i; j >= 0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
